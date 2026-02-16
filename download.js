9d4dcd992e1e035ca4fe71047246a3a299fd9286
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-02-16 16:57:27 +0800

    Calculate eta

--- download.js
+++ download.js
@@ -104,7 +104,14 @@ for (fileIndex = 0; fileIndex < fileArr.length; ++fileIndex) {
 			page.click(`a.node-download-btn[data-node="${node}"]`),
 		]);
 		if (downloadWillBeginFired) {
-			console.log(`Downloading file ${file.index}, id = ${file.id}, size = ${file.size}, guid = ${file.guid}, hostname = ${file.hostname}, suggestedFilename = ${file.suggestedFilename}`);
+			const [ sizeValue, sizeUnit ] = file.size.split(' ');
+			const etaInSeconds = sizeValue * Math.pow(sizeUnitK, sizeUnitArr.indexOf(sizeUnit) - 1) / 40; // Download rate is limited to 40 KB/s by *-data.bego.cc.
+			const etaInMinutes = etaInSeconds / 60;
+			const etaInHours = etaInMinutes / 60;
+			const etaH = Math.floor(etaInHours);
+			const etaM = etaInMinutes - 60 * etaH;
+			file.eta = `${new Date(Date.now() + 1000 * etaInSeconds).toLocaleTimeString('zh-CN')} (${etaH}h${etaM.toFixed(0)}m)`;
+			console.log(`Downloading file ${file.index}, id = ${file.id}, size = ${file.size}, eta = ${file.eta}, guid = ${file.guid}, hostname = ${file.hostname}, suggestedFilename = ${file.suggestedFilename}`);
 			break;
 		}
 		await new Promise(r => setTimeout(r, 5000)); // Pause for a while before retrying.
