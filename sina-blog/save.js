efe88b21cc32c2439c2b85b3c0beb4ff49bb42d5
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-02-17 18:24:57 +0800

    Promise.race(['转存文件', '返回上一页'])

--- sina-blog/save.js
+++ sina-blog/save.js
@@ -13,13 +13,12 @@ browser.setCookie(...[{
 }]);
 const [ page ] = await browser.pages();
 for (const c of cArr) {
+	console.log(c);
 	await page.goto(c);
-	let timeout = false;
-	await page.waitForSelector('button::-p-text(转存文件)', { timeout: 4000 }).catch(() => {timeout = true}); // Error 404
-	if (timeout) {
-		console.log(`Timeout:`, c);
-		continue;
-	}
+	await Promise.race(['转存文件', '返回上一页'].map(buttonText => page.waitForSelector(`button::-p-text(${buttonText})`)));
+	const title = await page.title();
+	console.log(title);
+	if (title === '找不到文件') continue;
 	await new Promise(resolve => setTimeout(resolve, 1000));
 	await page.evaluate(f => { f ? file_save() : bulk_file_save() }, c.split('/')[3] === 'f');
 	await new Promise(resolve => setTimeout(resolve, 2000));
