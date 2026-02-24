8c11bb70202df3b1a9f4603d2e6cf6aa67ade534
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-02-24 10:24:00 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+echo "$(date +"%F %T.%N") Script started"
+export PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium
+echo "$(date +"%F %T.%N") cd aiyiny"
+cd aiyiny
+echo "$(date +"%F %T.%N") node save.js"
+node save.js
+echo "$(date +"%F %T.%N") cd .."
+cd ..
+echo "$(date +"%F %T.%N") node download.js"
+node download.js
+echo "$(date +"%F %T.%N") cd downloads"
+cd downloads
+echo "$(date +"%F %T.%N") ./split-wav-to-flac.sh"
+./split-wav-to-flac.sh
+echo "$(date +"%F %T.%N") cd .."
+cd ..
+echo "$(date +"%F %T.%N") node upload.js"
+node upload.js
+echo "$(date +"%F %T.%N") Script completed"
