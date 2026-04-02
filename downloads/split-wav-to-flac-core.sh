6baa0803998d90498b4fb22711d3a1cb6bd8a40a
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-04-02 21:39:05 +0800

    Create split-wav-to-flac.sh

---
+++ downloads/split-wav-to-flac-core.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+wavfile=$(ls -1 *.wav)
+stem="${wavfile%.*}"
+iconv -f gb18030 -t utf8 "$stem.cue" | sed 's/（珍藏老歌 http:\/\/blog.sina.com.cn\/u\/1766037260）//g' > tmp.cue
+echo >> tmp.cue # Some cue files lack an empty line in the end, causing cueprint error: unable to parse input file. So append a new line.
+mv -f tmp.cue "$stem.cue" # Some "$stem.cue" are in read-only mode. mv without -f will issue a warning "overriding mode 0444" requiring manual intervention.
+shnsplit -f "$stem.cue" -t "%n %t" -o flac -q "$stem.wav"
+rm -f "00 pregap.flac" # Delete this possibly-existed dummy file, which was generated if TRACK 01 has non-zero INDEX 01 00:00:32
+cuetag "$stem.cue" *.flac
+wavbytecnt=$(stat -c %s "$stem.wav")
+flacbytecnt=$(du -bc *.flac | tail -n 1 | cut -f1)
+ratio=$((100 * flacbytecnt / wavbytecnt)) # bash can only do integer division.
+echo "flac/wav ratio $ratio% = $flacbytecnt / $wavbytecnt"
+if [ $ratio -lt 40 ] || [ $ratio -gt 80 ]; then
+	echo "Abnormal ratio: $ratio"
+else
+	rm "$stem.wav"
+fi
+for flacfile in *.flac; do
+	if [[ $flacfile == *:*.flac || $flacfile == *\?*.flac || $flacfile == *\"*.flac || $flacfile == *\**.flac ]]; then # If the flac filename contains colon : or question mark ? or double quotation mark " or asterisk *, delete them, because they are forbidden in alipan.
+		mv "$flacfile" "$(echo "$flacfile" | sed 's/://g' | sed 's/?//g' | sed 's/"//g' | sed 's/*//g')"
+	fi
+done
---
+++ downloads/split-wav-to-flac.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+dot='.'
+rarcnt=0
+for rarfile in *.rar; do
+	rarcnt=$((rarcnt+1))
+	echo "Procesesing rar file $rarcnt: $rarfile"
+	artist=${rarfile%%$dot*} # Extract the part before the substring
+	mkdir -p "$artist" # artist could contain &, e.g. 许冠杰&谭咏麟
+	cd "$artist"
+	$RARLINUX_ROOT/unrar -idq x ../"$rarfile"
+	cd "$(ls -1t --time=birth | head -n 1)" # cd into the directory with the latest creation/birth time.
+	wavcnt=$(ls -1 *.wav | wc -l)
+	echo "Found $wavcnt wav files"
+	if [[ $wavcnt -eq 0 ]]; then
+		if [[ $rarfile != *【FLAC*.rar ]]; then # 【FLAC分轨】 or 【FLAC+CUE】
+			echo "Keyword 【FLAC not found in filename"
+		fi
+	elif [[ $wavcnt -eq 1 ]]; then
+		../../split-wav-to-flac-core.sh
+	else
+		for (( i=1; i<=$wavcnt; i++ )); do
+			mkdir "CD$i"
+			cd "CD$i"
+			mv ../*CD$i*.* .
+			../../../split-wav-to-flac-core.sh
+			cd ..
+		done
+	fi
+	cd ../..
+#	gio trash $rarfile
+	echo
+done
+echo "wav file count = $(find -name *.wav | wc -l)"
+echo "rarcnt = $rarcnt"
+echo "dircnt = $(find -mindepth 2 -maxdepth 2 -type d | wc -l)"
